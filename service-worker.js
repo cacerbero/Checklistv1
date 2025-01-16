db2b92c94f2bd050cfda2ff3841904e146800af4
Carlos <carlosalberto.carmona@gmail.com>
2025-01-16 00:34:21 -0500

    updated service worker js with error handling

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,34 @@
-const CACHE_NAME = 'to-do-pwa-cache-v1';
+const CACHE_NAME = "to-do-pwa-cache-v1";
 const FILES_TO_CACHE = [
-    '/Checklist/',
-    'Checklist/index.html',
-    'Checklist/style.css',
-    'Checklist/app.js',
-    'Checklist/manifest.json',
-    'Checklist/icons/icon-128.png',
-    'Checklist/icons/icon-512.png'
+  "/Checklist/",
+  "Checklist/index.html",
+  "Checklist/style.css",
+  "Checklist/app.js",
+  "Checklist/manifest.json",
+  "Checklist/icons/icon-128.png",
+  "Checklist/icons/icon-512.png",
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(FILES_TO_CACHE)
+            .catch(() => {
+                console.error('Caching failed:', error);
+            });
+        })
+        .catch((error) => {
+            console.error('Cache open failed', error);
+
+        })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => response || fetch(event.request))
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
 });
